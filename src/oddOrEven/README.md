9fa2a80d46feff5ce1ec0923720ba0e49ab75c6c
Amelie Pira <amelie.pira@gmail.com>
2022-06-08 19:10:17 +0100

    intial folder setup

---
+++ src/oddOrEven/README.md
@@ -0,0 +1,3 @@
+# Odd or Even
+
+In this activity you will be writing code to create a function that determines whether a number is odd or even.
---
+++ src/oddOrEven/oddOrEven.test.js
@@ -0,0 +1,7 @@
+describe("oddOrEven", () => {
+  it("should return a string even for the number 30", () => {});
+
+  it("should return a string odd for the number 17", () => {});
+
+  it("should return a string even for the number 0", () => {});
+});
