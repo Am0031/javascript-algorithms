ddcf97ab8ba2d4764a48847de7094ea816f01693
Amelie Pira <amelie.pira@gmail.com>
2022-10-04 15:55:53 +0100

    refactored with ternary operator

--- src/vowelCount (todo)/vowelCount.js
+++ src/vowelCount (todo)/vowelCount.js
@@ -1,9 +1,7 @@
-const vowelCount = (str) => {
-  //check input
-  if (typeof str !== "string") return -1;
-
-  //return length
-  return str.replace(/[^(aeiou)]/gi, "").length;
-};
+const vowelCount = (str) =>
+  //check if input if of type string
+  //if string, use Regex to filter out the non vowel characters, and return length of the result(the string of only vowels)
+  //if not a string, return -1
+  typeof str === "string" ? str.replace(/[^(aeiou)]/gi, "").length : -1;
 
 module.exports = { vowelCount };
