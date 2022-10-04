5b23cfad72b2bfa1201d7bcc97e4fb38161349d8
Amelie Pira <amelie.pira@gmail.com>
2022-10-04 15:49:02 +0100

    adjusted function with regex

--- src/vowelCount (todo)/vowelCount.js
+++ src/vowelCount (todo)/vowelCount.js
@@ -1,14 +1,9 @@
 const vowelCount = (str) => {
   //check input
   if (typeof str !== "string") return -1;
-  //define vowels
-  const regex = /[aeiou]/i;
-
-  //iterate through string
-  const containedVowels = [...str].filter((c) => regex.test(c));
 
   //return length
-  return containedVowels.length;
+  return str.replace(/[^(aeiou)]/gi, "").length;
 };
 
 module.exports = { vowelCount };
