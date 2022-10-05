3ad34ebe65843cfcca6250b92d2206c19f9a95b9
Amelie Pira <amelie.pira@gmail.com>
2022-10-05 15:19:50 +0100

    adjusted test

--- src/mostFrequent/mostFrequent.test.js
+++ src/mostFrequent/mostFrequent.test.js
@@ -7,16 +7,4 @@ describe("mostFrequent", () => {
     const result = mostFrequent(array);
     expect(result).toEqual(expected);
   });
-  it("should return the number 966 when given the array of numbers [4, 8, 15, 16, 23, 42] ", () => {
-    const expected = 42;
-    const array = [4, 8, 15, 16, 23, 42];
-    const result = mostFrequent(array);
-    expect(result).toEqual(expected);
-  });
-  it("should return the number 966 when given the array of numbers [-10, -5, -2, -15, -1, -33, -88, -100] ", () => {
-    const expected = -1;
-    const array = [-10, -5, -2, -15, -1, -33, -88, -100];
-    const result = mostFrequent(array);
-    expect(result).toEqual(expected);
-  });
 });
