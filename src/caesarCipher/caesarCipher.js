8acce4ef7bf7c7cb3e6db7b237ab0936baac9a3c
Amelie Pira <amelie.pira@gmail.com>
2022-11-12 15:51:12 +0000

    amended notes

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -18,17 +18,20 @@ const caesarCipher = (string, offset) => {
     //prepare the character in a variable
     let offsetChar = char;
 
-    //check charCode value to see if it is a lower case or upper case letter. If so, offset it
+    //check charCode value to see if it is a letter. If so, offset it.
+    //if character is an upper case letter
     if (charCode >= 65 && charCode <= 90) {
       offsetChar = String.fromCharCode(((charCode - 65 + offset) % 26) + 65);
+      //if character is a lower case letter
     } else if (charCode >= 97 && charCode <= 122) {
       offsetChar = String.fromCharCode(((charCode - 97 + offset) % 26) + 97);
     }
 
-    //add the character to the result string
+    //add the character to the result string (other characters will be added as is, like special characters or spaces)
     result += offsetChar;
   }
 
+  //return the result with all characters reviewed
   return result;
 };
 
