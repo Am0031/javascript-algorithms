13385bfc8bfb78c9460c760d134b90338e233f1e
Amelie Pira <amelie.pira@gmail.com>
2024-02-18 18:29:57 +0000

    optimised loop

--- src/totalGoals/totalGoals.js
+++ src/totalGoals/totalGoals.js
@@ -12,15 +12,21 @@ const totalGoals = (str) => {
   //start with a count of goals at 0
   let goals = 0;
 
-  //map through the rounds array and the nested matches array and add the goals number when there's a string match
-  data.forEach((round) =>
-    round.matches.forEach((match) => {
+  //go through the rounds array, each team plays once in each round
+  //for each round, go through the matches array, once there's a string match, add the gools to the count and it can exit the loop
+  data.forEach((round) => {
+    let found = false;
+    let matchIndex = 0;
+    while (!found) {
+      const match = round.matches[matchIndex];
       if (match.team1.key === str || match.team2.key === str) {
         if (match.team1.key === str) goals += match.score1;
         if (match.team2.key === str) goals += match.score2;
+        found = true;
       }
-    })
-  );
+      matchIndex++;
+    }
+  });
 
   //return the total number of goals
   return goals;
